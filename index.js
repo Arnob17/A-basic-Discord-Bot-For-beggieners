b2f10131d7e06a7abb43b6a1c7f4786a78518eaa
Arnob !! <78209407+Arnob17@users.noreply.github.com>
2021-11-11 12:48:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const mybot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.once('ready', () => {
-console.log('am online!');
-
+mybot.on("ready", () => {
+  console.log(`${mybot.user.username} is ready!`);
 });
 
-
-
-client.on('message',msg => {
-if (msg.content === 'ping'){
-msg.channel.send('pong !');
-}
+mybot.on("messageCreate", (m) => {
+  if (m.content == "ping") {
+    m.reply("Pong");
+  };
 });
 
-
-
-client.login('TOKEN-HERE'); //U CAN GET BOT TOKEN BY GOING TO DISCORD DEVELOPER PORTAL 
-//CODED BY Arnob !!#1919
+mybot.login("usually token goes here but recommending process.env.token");
